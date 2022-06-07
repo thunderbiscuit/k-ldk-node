d808328a4bc53c71d5cadd72b2bb015ce0d3a3cb
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-06-07 16:20:40 -0300

    Add comment to Node singleton

--- app/src/main/kotlin/me/thunderbiscuit/kldk/Node.kt
+++ app/src/main/kotlin/me/thunderbiscuit/kldk/Node.kt
@@ -5,6 +5,9 @@ import org.ldk.batteries.NioPeerHandler
 import org.ldk.structs.ChannelManager
 import org.ldk.structs.PeerManager
 
+// the Node singleton is really just a way to namespace and keep a reference to
+// the peerHandler, peerManager, and channelManager so we can use them throughout the app
+// and they don't get garbage collected
 object Node {
 
     private val MuLogger = KotlinLogging.logger("BaseLogger")
