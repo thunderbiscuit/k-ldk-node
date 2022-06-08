35c1cf59cf626696ccb56bee4cbd8f6511c7d5a9
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-06-08 15:04:08 -0300

    Add broadcast implementation using Blockstream API

--- app/src/main/kotlin/me/thunderbiscuit/kldk/network/Blocks.kt
+++ app/src/main/kotlin/me/thunderbiscuit/kldk/network/Blocks.kt
@@ -5,6 +5,12 @@ import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import me.thunderbiscuit.kldk.utils.toHex
+
+// Blockstream API docs at https://github.com/Blockstream/esplora/blob/master/API.md
+
+// All those calls should eventually be performed using the Bitcoin Dev Kit, which would
+// enable more flexibility (choice of bitcoin daemon, electrum server, etc.)
 
 suspend fun getLatestBlockHash(): String {
     val client = HttpClient(CIO)
@@ -23,4 +29,10 @@ suspend fun getLatestBlockHeader(): ByteArray {
     val blockHash: String = getLatestBlockHash()
     val response: HttpResponse = client.get("https://blockstream.info/testnet/api/block/$blockHash/header")
     return response.body<String>().toByteArray()
+}
+
+suspend fun blockstreamBroadcastTx(tx: ByteArray): String {
+    val client = HttpClient(CIO)
+    val response: HttpResponse = client.post("https://blockstream.info/testnet/api/tx/${tx.toHex()}")
+    return response.body<String>()
 }
--- app/src/main/kotlin/me/thunderbiscuit/kldk/node/KldkBroadcaster.kt
+++ app/src/main/kotlin/me/thunderbiscuit/kldk/node/KldkBroadcaster.kt
@@ -1,9 +1,19 @@
 package me.thunderbiscuit.kldk.node
 
+import kotlinx.coroutines.*
+import me.thunderbiscuit.kldk.network.blockstreamBroadcastTx
+import mu.KLogger
+import mu.KotlinLogging
 import org.ldk.structs.BroadcasterInterface
 
 object KldkBroadcaster : BroadcasterInterface.BroadcasterInterfaceInterface {
     override fun broadcast_transaction(tx: ByteArray?): Unit {
-        println(tx.toString())
+        tx?.let {
+            CoroutineScope(Dispatchers.IO).launch {
+                val txid: String = blockstreamBroadcastTx(tx)
+                val logger: KLogger = KotlinLogging.logger("BaseLogger")
+                logger.info { "We've broadcast a transaction with txid $txid" }
+            }
+        } ?: throw(IllegalStateException("Broadcaster attempted to broadcast an null transaction"))
     }
 }
