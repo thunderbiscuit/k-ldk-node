74942a6f279134cc5d4411f2c2c3bf66c57d793e
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-06-07 21:30:00 -0300

    Add timer to check Node singleton doesn't get garbage collected

--- app/src/main/kotlin/me/thunderbiscuit/kldk/Node.kt
+++ app/src/main/kotlin/me/thunderbiscuit/kldk/Node.kt
@@ -4,13 +4,18 @@ import mu.KotlinLogging
 import org.ldk.batteries.NioPeerHandler
 import org.ldk.structs.ChannelManager
 import org.ldk.structs.PeerManager
+import java.util.Timer
+import kotlin.concurrent.schedule
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.seconds
 
-// the Node singleton is really just a way to namespace and keep a reference to
+// the Node singleton is really just a way to namespace and keep references to
 // the peerHandler, peerManager, and channelManager so we can use them throughout the app
 // and they don't get garbage collected
 object Node {
 
-    private val MuLogger = KotlinLogging.logger("BaseLogger")
+    // private val MuLogger = KotlinLogging.logger("BaseLogger")
+    private val timer = Timer(true)
 
     val peerHandler: NioPeerHandler
     val peerManager: PeerManager
@@ -18,13 +23,19 @@ object Node {
 
     init {
         val coreNodeElements: CoreNodeElements = startNode()
-        // peerHandler, peerManager, channelManager = startNode() // doesn't work
         peerHandler = coreNodeElements.peerHandler
         peerManager = coreNodeElements.peerManager
         channelManager = coreNodeElements.channelManager
     }
 
     fun initialize() {
-        MuLogger.info { "Node object initialized" }
+        // MuLogger.info { "Node object initialized" }
+        println("Node object initialized...")
+        timer.schedule(
+            delay = 0.seconds.inWholeMilliseconds,
+            period = 10.seconds.inWholeMilliseconds
+        ) {
+            println("Object is still alive")
+        }
     }
 }
